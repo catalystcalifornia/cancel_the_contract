ca9a391ee7f28b5d74ae0c90b89b6d2a9a7cdcf3
Jennifer Zhang <JZhang@catalystcalifornia.org>
2025-06-25 15:43:12 -0700

    finalize race script and push table to postgres

--- Data Prep/av_race_pop.R
+++ Data Prep/av_race_pop.R
@@ -57,7 +57,7 @@ colnames(soasian)<-tolower(names(soasian))
 ## Calculate South Asian Count in AV
 soasian_av<-soasian%>%
   filter(geoid %in% av_ct$tract_geoid)%>%
-  group_by(name,geoid)%>%
+  group_by(geoid)%>%
   summarise(soasian_count=sum(estimate),
             soasian_moe=moe_sum(moe,estimate))
 
@@ -65,13 +65,13 @@ soasian_av<-soasian%>%
 # Calculate race estimates for AV ----
 
 ##### standard race groups  ------
-av<-df%>%
+av_race<-df%>%
   filter(geoid %in% av_ct$tract_geoid)
 
 ##### asian minus south asian  ------
 # reference for calcs using tidycensus https://psrc.github.io/psrccensus/articles/calculate-reliability-moe-transformed-acs.html
 # get nh asian counts from dp05 table and join to soasian counts
-asian_wo_soasian<-av%>%filter(variable=='nh_asian')%>%left_join(soasian_av)
+asian_wo_soasian<-av_race%>%filter(variable=='nh_asian')%>%left_join(soasian_av, by=c("geoid"="geoid"))
 
 # calculate new count for nh asian without south asian
 asian_wo_soasian<-asian_wo_soasian%>%
@@ -91,8 +91,8 @@ asian_wo_soasian<-asian_wo_soasian%>%
          moe=new_moe)
 
 ##### add in percentages out of total and cvs -----
-df_final<-rbind(city,asian_wo_soasian)%>%
-  left_join(city%>%
+df_final<-rbind(av_race,asian_wo_soasian)%>%
+  left_join(av_race%>%
               select('geoid','name','variable','count')%>%
               rename('total'='variable')%>%rename('population'='count')%>%
               filter(total=='total'),
@@ -101,42 +101,61 @@ df_final<-rbind(city,asian_wo_soasian)%>%
          cv=moe/1.645/count*100)%>%
   select('geoid','name','variable','count','moe','rate','cv')
 
-# Finalize and push table to postgres --------------------------------
+# Remove SWANA since we don't need it 
 df_final<-df_final%>%
   rename('race'='variable')%>%
   filter(race!='swana') # don't need swana for ripa estimates just sswana
 
+# Aggregate tracts up to Antelope Valley level --------------------------------
+
+df_final <- df_final %>%
+  group_by(race) %>%
+  summarise(
+    count = sum(count),
+    moe = sqrt(sum(moe^2)),        # Aggregate MOE using square root of sum of squares
+    .groups = "drop"
+  ) %>%
+  mutate(
+    total = count[race == "total"],
+    rate = count / total,
+    cv = moe / (1.645 * count),   # Recalculate CV from new MOE and count
+    geography = "Antelope Valley"
+  ) %>%
+  select(geography, race, total, count, rate, moe, cv)%>%
+  filter(race!="total")
+
+# Push table to postgres----------------------------
+
 # set field types for postgresql db
 charvect = rep('varchar', ncol(df_final)) #create vector that is "numeric" for the number of columns in df
 
-charvect <- replace(charvect, c(4,5,6,7), c("numeric"))
+charvect <- replace(charvect, c(3,4,5,6,7), c("numeric"))
 
 # add df colnames to the character vector
 
 names(charvect) <- colnames(df_final)
 
-dbWriteTable(con,  "population_race_fresno_city", df_final, 
-             overwrite = FALSE, row.names = FALSE,field.types = charvect
-)
+table_name <- "av_population_race"
+schema <- 'data'
+indicator <- "2023 ACS Race estimates and rates for Antelope Valley"
+source <- "AV to census tract xwalk from the Region 5 State of the Child report. Race data from ACS
+imported to rda_shared_data."
 
-dbSendQuery(con, paste0("comment on table population_race_fresno_city is 
-                        'Population estimates and percentages for Fresno city by race for non-Hispanic categories, Latinx, and all-AIAN, all-NHPI, and all-SSWANA(coded as swana) based on DP05 and B04006(ancestry) for sswana and B02018 for south asian. 
-                        Prefix nh indicates that it is a non-Hispanic/Latinx category.
-                        Added nh_asian_wo_sa as an additional category for testing which removes south asian alone individuals from nh_asian using B02015 estimates
-                         See W:\\Project\\ECI\\Fresno RIPA\\R\\Data and Geo Prep\\population_estimates.R and
-                        W:\\Project\\ECI\\Fresno RIPA\\Documentation\\QA_population_estimates.docx for more info';"))
+dbWriteTable(con, Id(schema, table_name), df_final,
+             overwrite = FALSE, row.names = FALSE)
 
-dbSendQuery(con, paste0("COMMENT ON COLUMN 
-                         population_race_fresno_city.race
-                         IS 
-                         'Racial group or total for which count, moe, or rate is for nh prefix indicates Latinx exclusive. aian, nhpi, and sswana include alone or in combination with another race including Latinx. SSWANA is based on ancestry data for mena and south asian alone or in combo. Nh_asian_wo_sa excludes south asian alone individuals from nh_asian estimates';
-                         COMMENT ON COLUMN 
-                         population_race_fresno_city.count
-                         IS 
-                         'Estimated count of racial group or total based on city level ACS data';
-                         COMMENT ON COLUMN 
-                         population_race_fresno_city.rate
-                         IS 
-                         'Estimated percent of racial group (in decimal format) out of total based on city level ACS data';"))
+qa_filepath <- "W:\\Project\\RJS\\CTC\\Documentation\\QA_av_race_pop.docx" 
 
+# comment on table and columns
+column_names <- colnames(df_final)
+column_comments <- c('geo level',
+                     'race',
+                     'total AV population',
+                     'count based on race group',
+                     'rate of race groups',
+                     'MOE',
+                     'CV')
+
+# add comment on table and columns using add_table_comments() (accessed via credentials script) 
+add_table_comments(con, schema, table_name, indicator, source, qa_filepath, column_names, column_comments) 
 
