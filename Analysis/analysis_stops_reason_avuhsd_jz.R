59a23c8f443275db51d304c962b1c42b54319b52
Jennifer Zhang <JZhang@catalystcalifornia.org>
2025-07-09 17:10:31 -0700

    add in offense codes statutes

--- Analysis/analysis_stops_reason_avuhsd_jz.R
+++ Analysis/analysis_stops_reason_avuhsd_jz.R
@@ -6,6 +6,7 @@ library(tidytext)
 # Connect to postgres
 source("W:\\RDA Team\\R\\credentials_source.R")
 con_rda <- connect_to_db("rda_shared_data")
+con_fresno <- connect_to_db("eci_fresno_ripa")
 con_ctc <- connect_to_db("cancel_the_contract")
 
 # Get LASD stop-level data for Antelope Valley high schools
@@ -31,19 +32,23 @@ ORDER BY school_name")
 # Get LASD person-level data
 lasd_stops_person_raw <- dbGetQuery(con_rda, "SELECT * FROM crime_and_justice.lasd_stops_person_2018_2023")
 
+# Grab offense codes from the Fresno RIPA db
+
+codes<-dbGetQuery(con_fresno, "SELECT * FROM cadoj_ripa_offense_codes_2023")
 
 # Join stop-level data with person-level data, such that each row represents one person.
 person_lasd <- lasd_stops_person_raw %>% 
   right_join(lasd_stops_incident_raw, by="contact_id")
 
-# Grab only person reason columns
-
-person_reason<-person_lasd%>%
-  select(contact_id, person_id, reason_for_contact, reason_for_contact_narrative, starts_with("reasonable_suspicion_"))
-
 # tabulate reasons
 
 reason_table<-as.data.frame(table(person_reason$reason_for_contact))
+
+# Grab only reasonable suspicion columns because it is the overwhelming majority of stop reasons
+
+person_reason<-person_lasd%>%
+  select(contact_id, person_id, reason_for_contact, reason_for_contact_narrative, starts_with("reasonable_suspicion_"), offense_code_of_the_reasonable_suspicion)
+
 
 # Exploration----------------
 
@@ -54,7 +59,19 @@ not_rs<-person_reason%>%
     !grepl("Reasonable suspicion", reason_for_contact),
     apply(select(., starts_with("reasonable_suspicion_")), 1, function(row) any(row %in% c("true", "Yes")))) # turns up 0 so we know those columns are clean
 
-# now lets focus in on reasonable suspicion
+# Now I want to test if any stops are listed 'reasonable suspicion' but NONE of the 1/0 reasonable suspicion columns are selected
+
+rs_no_dummy<-person_reason %>%
+  filter(reason_for_contact == "reasonable suspicion") %>%
+  group_by(person_id, contact_id)%>%
+  filter(
+    if_all(starts_with("reasonable_suspicion"), 
+           ~ !(. %in% c("Yes", "true", TRUE)))
+  )
+
+# this turns up zero cases
+
+# now lets focus in on reasonable suspicion sub-types
 
 rs<-person_reason%>%
   filter(grepl("Reasonable suspicion", reason_for_contact))
@@ -69,10 +86,32 @@ rs_re<-rs%>%
   select(-reason_for_contact_narrative)%>%
   pivot_longer(4:11, names_to = "reasonable_suspicion_reason", values_to = "value")%>%
   filter(value==1)%>%
-  group_by(reasonable_suspicion_reason)%>%
-  mutate(count=sum(value))%>%
-  slice(1)%>%
-  select(reasonable_suspicion_reason, count)%>%
-  arrange(-count)
+  select(-value)
+
+# Left join the offense codes tables so we can get descriptions
+
+rs_re_codes<-rs_re%>%
+  left_join(codes, by=c("offense_code_of_the_reasonable_suspicion"="offense_statute"))
+
+# see what did not join
+
+nonjoin<-rs_re_codes%>%
+  filter(is.na(statute_literal_25))  # none of these 27 people had a statute to go with their reasonable suspicion stop
+
+table(nonjoin$reasonable_suspicion_reason) # see what reasonable suspicion column was selected for these
+
+
+# Continue exploring the stops that did join to the offense code description---------
+
+# note there is a many to many relationship to the join because some offense statutes have different actual offense codes and some are M/I/F
+
+# look at the different reasonable suspicion subtypes
+table(rs_re$reasonable_suspicion_reason) # majority are reasonable_suspicion_person_witness_or_victim_ofsuspect: 662
+
+# lets look at person witness or victim of suspect since thats the majority of the reasonable suspicions
+
+
+rs_witness_victim<-rs_re_codes%>%
+  filter(reasonable_suspicion_reason=="reasonable_suspicion_person_witness_or_victim_ofsuspect")
 
 # Explore raw meta data here: https://data.lacounty.gov/datasets/lacounty::sheriff-officer-contacts-person-details-/about 
