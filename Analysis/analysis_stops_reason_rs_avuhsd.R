ad52cf5b09275e92d731025a6bb1504c6e961af8
Jennifer Zhang <JZhang@catalystcalifornia.org>
2025-07-10 11:57:16 -0700

    finalize script and push ti postgres

--- Analysis/analysis_stops_reason_rs_avuhsd.R
+++ Analysis/analysis_stops_reason_rs_avuhsd.R
@@ -45,15 +45,15 @@ person_lasd <- lasd_stops_person_raw %>%
 
 # tabulate reasons
 
-reason_table<-as.data.frame(table(person_reason$reason_for_contact))
+reason_table<-as.data.frame(table(person_lasd$reason_for_contact))
 
-# Grab only reasonable suspicion columns because it is the overwhelming majority of stop reasons
+# Grab the reasonable suspicion columns because it is the overwhelming majority of stop reasons
 
 person_reason<-person_lasd%>%
   select(contact_id, person_id, reason_for_contact, reason_for_contact_narrative, starts_with("reasonable_suspicion_"), offense_code_of_the_reasonable_suspicion)
 
 
-# Exploration----------------
+# Data checks----------------------
 
 # First I am curious if any stops can be listed for NOT 'reasonable suspicion' but still have any of the subsequent reasonable suspicion columns marked as true
 
@@ -61,6 +61,8 @@ not_rs<-person_reason%>%
   filter(
     !grepl("Reasonable suspicion", reason_for_contact),
     apply(select(., starts_with("reasonable_suspicion_")), 1, function(row) any(row %in% c("true", "Yes")))) # turns up 0 so we know those columns are clean
+
+rm(not_rs)
 
 # Now I want to test if any stops are listed 'reasonable suspicion' but NONE of the 1/0 reasonable suspicion columns are selected
 
@@ -74,7 +76,11 @@ rs_no_dummy<-person_reason %>%
 
 # this turns up zero cases
 
-# now lets focus in on reasonable suspicion sub-types
+rm(rs_no_dummy)
+
+# Now lets focus in on reasonable suspicion stop reasons and the sub-types---------------------------
+
+# grab only stop where the reason was reasonable suspicion
 
 rs<-person_reason%>%
   filter(grepl("Reasonable suspicion", reason_for_contact))
@@ -91,7 +97,11 @@ rs_re<-rs%>%
   filter(value==1)%>%
   select(-value)
 
-# Left join the offense codes tables so we can get descriptions
+# look at tabulation of the subtype of reasonable suspicions
+
+rs_table<-as.data.frame(table(rs_re$reasonable_suspicion_reason))
+
+# Left join the Fresno RIPA offense codes tables so we can get descriptions for the reasonable suspicion statutes
 
 rs_re_codes<-rs_re%>%
   left_join(codes, by=c("offense_code_of_the_reasonable_suspicion"="offense_statute"))
@@ -103,6 +113,19 @@ nonjoin<-rs_re_codes%>%
 
 table(nonjoin$reasonable_suspicion_reason) # see what reasonable suspicion column was selected for these
 
+# bring back the reason_narrative column to see if any of those contain the offense codes
+
+nonjoin<-nonjoin%>%left_join(person_reason%>%select(contact_id, person_id, reason_for_contact_narrative), by=c("contact_id", "person_id"))%>%
+  mutate(offense_code_of_the_reasonable_suspicion=reason_for_contact_narrative)
+
+# we can see now more offense codes get filled in. Lets clean up the offense_code column a bit
+
+nonjoin<-nonjoin%>%
+  select(1:5)%>%
+  mutate(ifelse())
+  
+
+
 
 # Continue exploring the stops that did join to the offense code description---------
 
@@ -111,23 +134,56 @@ table(nonjoin$reasonable_suspicion_reason) # see what reasonable suspicion colum
 # look at the different reasonable suspicion subtypes
 table(rs_re$reasonable_suspicion_reason) # majority are reasonable_suspicion_person_witness_or_victim_ofsuspect: 662
 
-# lets look at person witness or victim of suspect since thats the majority of the reasonable suspicions
 
+# ANALYSIS 1: Calculate Counts and Rates of the Reasonable Suspicion offense codes WITHIN each Reasonable Suspicion subtype---------
 
-rs_witness_victim<-rs_re_codes%>%
-  filter(reasonable_suspicion_reason=="reasonable_suspicion_person_witness_or_victim_ofsuspect")
-
-# Rate: Offense codes / all citations WITHIN reasonable suspicion
-
-df1<-rs_re_codes%>%
+df<-rs_re%>%
   group_by(reasonable_suspicion_reason)%>%
   mutate(total=n())%>%
-  group_by(reasonable_suspicion_reason, statute_literal_25, offense_type_of_charge)%>%
+  left_join(codes, by=c("offense_code_of_the_reasonable_suspicion"="offense_statute"))%>%
+  group_by(reasonable_suspicion_reason, offense_code_of_the_reasonable_suspicion, statute_literal_25, offense_type_of_charge)%>%
   mutate(count=n(),
          rate=count/total*100)%>%
   slice(1)%>%
   ungroup()%>%
-  select(reason_for_contact, reasonable_suspicion_reason, statute_literal_25, offense_type_of_charge, total, count, rate)%>%
+  select(reason_for_contact, reasonable_suspicion_reason, offense_code_of_the_reasonable_suspicion, 
+         statute_literal_25, offense_type_of_charge, total, count, rate)%>%
   arrange(reasonable_suspicion_reason, -rate)
 
+# clean up columns
 
+df<-df%>%
+  mutate(reasonable_suspicion_reason = str_replace_all(reasonable_suspicion_reason, "_", " "),
+         reasonable_suspicion_reason = str_remove(reasonable_suspicion_reason, "reasonable suspicion "))
+
+# Push table to postgres--------------------------------------
+
+# Write table with metadata
+table_name <- "analysis_stops_reason_rs_avuhsd"
+schema <- "data"
+indicator<- "This is a long form table looking specifically at peopel stopped in AVUHSD where the reason for the stop was
+reasonable suspicion. It then calculates the counts and rates of each reasonable suspicion offense code WITHIN each specific reasonable suspicion sub-type.
+For exampple, for reasonable suspicion: action indicative violentcrime, we calculate the different reasonable suspicion offense codes (numerator) / total reasonable suspicion indicative of a violent crime stops (denominator).
+We left join the CADOJ offense code table to get specific statute descriptions that correspond with the reasonable suspicion offense codes. 
+There is a many to many relationship between the reasonable suspicion offense codes and the CADOJ offense code table because the same reasonable suspicion offense codes 
+can either be a Misdemeanor/Infraction/Felony and can also have slightly different statute descriptions. The total/count/rate values in the table do not duplicate based off this many to many join
+but in order to examine the different statute descriptions rows are duplicated to reflect this many to many relationship."
+source <- "Script: W:/Project/RJS/CTC/Github/AV/cancel_the_contract/Analysis/analysis_stops_reason_rs_avuhsd.R"
+qa_filepath <- "See QA doc for details: W:/Project/RJS/CTC/Documentation/QA_analysis_stops_reason_rs_avuhsd.docx"
+table_comment <- paste0(indicator, source)
+dbWriteTable(con_ctc, Id(schema, table_name), df, overwrite = TRUE, row.names = FALSE)
+
+# Comment on table and columns
+column_names <- colnames(df) # Get column names
+column_comments <- c(
+  "Stop reason (all will be reasonable suspicion)",
+  "Sub-reasonable suspicion reason type",
+  "Offense code for reasonable suspicion stop",
+  "Description of reasonable suspicion offense code",
+  "Type of offense charge ( I = Infraction, M = Misdemeanor, F = Felony)",
+  "Denominator: The total number of students stopped in the school or district for the reasonable suspicion sub-reason type",
+  "Numerator: The number of students stopped for the specific combination of reasonable suspicion sub-reason type and reasonable suspicion  offense code",
+  "The rate represents the percentage of reasonable suspicion offense codes within the specific reasonable suspicion reason type"
+)
+
+ add_table_comments(con_ctc, schema, table_name, indicator, source, qa_filepath, column_names, column_comments)
