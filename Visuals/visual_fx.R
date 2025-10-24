a88f7ac781a17f33eb522f0e665f4a42ad9278d0
Jennifer Zhang <JZhang@catalystcalifornia.org>
2025-10-24 16:26:58 -0700

    cleaning up visual functions and incorporating CTC styling

--- Visuals/visual_fx.R
+++ Visuals/visual_fx.R
@@ -36,21 +36,110 @@ lightblue<-"#bae7fc"
 black <- "#000000"
 textgrey <- "#919191"
 
+# Add fonts
+
+# Steps: 
+# 1) Download the zip file from https://font.download/font/gill-sans-2 & https://font.download/font/placard-mt-std
+# 2) Extract the zip file and drag them into your local C:\\Windows\\Fonts folder
+# 3) Run this code to get the exact file name:
+#####systemfonts::system_fonts() |> dplyr::filter(grepl("Gill", family, ignore.case = TRUE))
+# 4) Use the exact file names for the font_add() code to add the fonts to R
+
+
+## Adding the gillsans fonts:
+
+# systemfonts::system_fonts() |> dplyr::filter(grepl("Gill", family, ignore.case = TRUE))
+
+font_add("gillsans", regular = "C:\\Windows\\Fonts\\GIL_____.TTF")
+
+font_add("gillsans_bold", regular = "C:\\Windows\\Fonts\\GILB____.TTF")
+
+## Adding the placard fonts:
+
+# library(systemfonts)
+# 
+# system_fonts_df <- system_fonts() %>%
+#   as.data.frame() %>%
+#   filter(grepl("Placard", family, ignore.case = TRUE)) %>%
+#   mutate(full_path = normalizePath(path, winslash = "/", mustWork = FALSE))
+# 
+# system_fonts_df
+
+font_add("placard", regular = "C:\\Users\\JZhang\\AppData\\Local\\Microsoft\\Windows\\Fonts\\PlacardNextRegular.TTF")
+
+font_add("placard_bold", regular = "C:\\Users\\JZhang\\AppData\\Local\\Microsoft\\Windows\\Fonts\\PlacardNextBold.TTF")
+
+
+# Use showtext_auto() to get fonts to load
+
+showtext_auto()
+
 # define fonts in chart
-font_title <- "placard_regular"
-font_subtitle <- "placard_regular_bold"
+
+font_title <- "placard"
+font_subtitle <- "placard_bold"
 font_caption <- "gillsans"
 font_bar_label <- "gillsans"
 font_axis_label <- "gillsans"
 
-font_table_title<-"Work Sans"
-font_table_text<-"Work Sans"
+font_table_title<-"placard"
+font_table_subtitle<-"placard_bold"
+font_table_text<-"gillsans"
+
+# Race Recoding FX-------------------------------------
+
+# This is so we don't have to constantly recode the race labels to be forward-facing
+
+race_recode<-function(df){
+  
+  df<-df%>%
+    filter(!label %in% c("nh_aian", "nh_nhpi", "nh_asian_wo_sa", "nh_sswana"))%>% # we can filter these groups out
+    mutate(label=ifelse(label %in% "latino", "Latinx",
+                        ifelse(label %in% "latinx", "Latinx",
+                        ifelse(label %in% "nh_white", "White",
+                               ifelse(label %in% "nh_black", "Black",
+                                      ifelse(label %in% "nh_asian", "Asian",
+                                             ifelse(label %in% "aian", "AIAN",
+                                                    ifelse(label %in% "AIAN AOIC", "AIAN",
+                                                    ifelse(label %in% "nh_twoormor", "Multiracial",
+                                                           ifelse(label %in% "sswana", "SSWANA",
+                                                                  ifelse(label %in% "SSWANA AOIC", "SSWANA",
+                                                                  ifelse(label %in% "nh_other", "Other",
+                                                                         ifelse(label %in% "nhpi", "NHPI",
+                                                                                ifelse(label %in% "NHPI AOIC", "NHPI",
+                                                                                       label)
+                                                                  )))))))))))))
+}
+                        
+# EX) RACE RECODING----------------------------------
+
+df<-dbGetQuery(con, "SELECT * FROM analysis_stops_race")%>%
+  rename(label=reportingcategory_re)
+  
+  
+ df_new<- race_recode(df)
+  
+
 
 # STATIC TABLE FX-----------------------------
 
 
-static_table <- function(df, indicator, title_text, footnote_text)
+static_table <- function(df, indicator, title_text)
 {
+ 
+  # Divide rate by 100
+  
+  df<-df%>%
+    mutate(across(contains("rate", ignore.case = TRUE), ~ .x / 100))
+  
+  ## Set up subtitle text: This will be from the data dictionary
+  
+  subtitle_text<-paste0(dict$indicator[dict$indicator_short==indicator])
+    
+  # # set caption text to use values from the data dictionary
+  
+  footnote_text<-paste0("Source: Catalyst California calculations of ",dict$source[dict$indicator_short==indicator]," data, ", dict$year[dict$indicator_short==indicator],". ",dict$method_note[dict$indicator_short==indicator])
+  footnote_text <- str_wrap(footnote_text, width = 110)
   
   # Conditionally rename 'label' column to 'Student Group' ONLY if the table is with education data
   
@@ -69,8 +158,47 @@ static_table <- function(df, indicator, title_text, footnote_text)
   
  final_visual<-gt(df) %>% 
     opt_all_caps() %>%
-    tab_header(title = md(paste0("**",title_text,"**"))) %>%
+   
+   
+   # use CSS file for fonts
+   opt_css(css = readLines("W:\\Project\\RJS\\CTC\\Github\\JZ\\cancel_the_contract\\Visuals\\styling.css"), add = TRUE) %>%
+   opt_table_font(font = font_table_text) %>%
+   
+    tab_header(title = md(title_text),
+               subtitle = md(paste0("**",subtitle_text,"**"))) %>%
     tab_footnote (footnote = md(footnote_text))%>% 
+   
+   #title font and text styling
+   
+   tab_style(
+     style = cell_text(font = font_table_title),
+     locations = cells_title(groups = "title")
+   ) %>%
+   
+   #subtitle font and text styling
+   
+   tab_style(
+     style = cell_text(font = font_table_title),
+     locations = cells_title(groups = "subtitle")
+   ) %>%
+   
+   fmt_number(
+     columns = where(is.numeric), # This will find and format ALL numeric columns to have comma separators
+     use_seps = TRUE,
+     decimals = 0 # Use this if you don't want decimal places
+   ) %>%
+   
+   # Add percent signs to rate column
+   
+   fmt_percent(
+     columns = matches("rate", ignore.case = TRUE),
+     decimals = 0
+   )%>%
+   # Style the column headers using the custom header font
+   tab_style(
+     style = cell_text(font = font_table_title),
+     locations = cells_column_labels()
+   ) %>%
     cols_align(
       align = c("left"),
       columns = everything()
@@ -84,7 +212,7 @@ static_table <- function(df, indicator, title_text, footnote_text)
     data_color(
       columns = matches("Rate"),
       colors = scales::col_numeric(
-        palette = c("white", lightblue),
+        palette = c("white", teal),
         domain = NULL,
         na.color = textgrey
       )
@@ -101,12 +229,14 @@ static_table <- function(df, indicator, title_text, footnote_text)
                 column_labels.border.bottom.color = black,
                 data_row.padding = px(3),
                 source_notes.font.size = 8,
+                heading.title.font.size = px(24),
+                heading.subtitle.font.size = px(22),
+                column_labels.font.size = px(18),
                 table.font.size = 16,
                 heading.align = "left",
                 container.width = 500
-    ) %>%
-    opt_table_font(font = list(google_font(name = font_table_text), font_title, font_caption ,default_fonts()))
-
+    ) 
+  
  # Define base file path for saving visuals
  base_path <- paste0("W:/Project/RJS/CTC/Visuals/",indicator, "_table")
  showtext_opts(dpi=300)
@@ -128,24 +258,22 @@ static_table <- function(df, indicator, title_text, footnote_text)
 df<-dbGetQuery( con, "SELECT * FROM analysis_suspensions")%>%
   select(label, enrollment_total, suspension_count, suspension_rate) # select columns you want in the table
 
-# define parameters outside of function ( these  can also be defined in the function)
+# NOTE: The indicator field needs to match the way it is in the data dictionary indicator_short column
+## i.e.) for suspensions by race, I need to set indicator== "Suspensions by race"
 
-indicator="suspensions"
-title_text="Suspension Rates by Student Group, Antelope Valley Union High School District, 2023-24"
-footnote_text="Source: Catalyst California calculations of California Department of Education data, 2023-24.
-Note: The student group category for non-binary students was unavailable because of a lack of data. AIAN stands for American Indian Alaskan Native."
-
+indicator="Suspensions by race"
+title_text="Marginalized Students are Suspended at Disproportionately High Rates"
+ 
 # Apply function
 
 static_table(df=df, 
              indicator=indicator, 
-             title_text=title_text,
-             footnote_text=footnote_text)
+             title_text=title_text)
 
 
 # SINGLE BAR GRAPH FUNCTION -------------------------------------
 
-single_bar<-function(df, indicator, title_text, subtitle_text, caption_text){
+single_bar<-function(df, indicator, title_text){
   
   # rename 'rate' column for function and arrange by rate descending
   df<-df%>%
@@ -153,6 +281,15 @@ single_bar<-function(df, indicator, title_text, subtitle_text, caption_text){
   
   # Define max value
   max_y = 1.15 * max(df$rate)
+  
+  ## Set up subtitle text: This will be from the data dictionary
+  
+  subtitle_text<-paste0(dict$indicator[dict$indicator_short==indicator])
+  
+  # # set caption text to use values from the data dictionary
+  
+  caption_text<-paste0("Source: Catalyst California calculations of ",dict$source[dict$indicator_short==indicator]," data, ", dict$year[dict$indicator_short==indicator],". ",dict$method_note[dict$indicator_short==indicator])
+  caption_text <- str_wrap(caption_text, width = 110)
 
     # Graph
   
@@ -169,7 +306,6 @@ single_bar<-function(df, indicator, title_text, subtitle_text, caption_text){
               family = font_bar_label, 
               hjust = -0.1,   # small negative number pushes text to the right of the bar
               vjust = 0.5,
-              fontface = "bold",  
               colour = "black") +
     
     labs(title = str_wrap(title_text, width = 65),
@@ -189,8 +325,8 @@ single_bar<-function(df, indicator, title_text, subtitle_text, caption_text){
                                      colour = black, family= font_axis_label),
           axis.text.x = element_blank(),
           plot.caption = element_text(hjust = 0.0, size = 8, colour = black, family = font_caption),
-          plot.title =  element_text(hjust = 0.0, size = 15, colour = black, family = font_title), 
-          plot.subtitle = element_text(hjust = 0.0, size = 12, colour = black, family = font_axis_label),
+          plot.title =  element_text(hjust = 0.0, size = 18, colour = black, family = font_title), 
+          plot.subtitle = element_text(hjust = 0.0, size = 12, colour = black, family = font_subtitle),
           axis.ticks = element_blank(),
           # grid line style
           panel.grid.minor = element_blank(),
@@ -219,36 +355,31 @@ single_bar<-function(df, indicator, title_text, subtitle_text, caption_text){
 
 df<-dbGetQuery(con, "SELECT * FROM av_population_race")
 
-indicator<-"race"
-title_text<-"Findings based title"
-subtitle_text<-"High School Graduation Rates by Student Subgroup in Antelope Valley Union High School District"
-caption_text<-"Source: California Department of Education, Adjusted Cohort Graduation Rate and Outcome Data,
-2023-2024. Note: Rates are out of 100 students. AIAN stands for American Indian and Alaskan Native."
+# first apply the race_recode function if you are visualizing something disaggregated by race
+# for it to work you need to rename your column that needs to be recoded to 'label'
+
+df<-df%>%
+  rename("label"="race")%>% # This is the column that needs to get renamed
+  race_recode() # apply race recoding
+
+# NOTE: The indicator field needs to match the way it is in the data dictionary indicator_short column
+## i.e.) for suspensions by race, I need to set indicator== "Suspensions by race"
+
+indicator<-"Race"
+title_text<-"The Majority of the Antelope Valley Population is Latinx, White or Black"
 
 # Apply function
 
-# make sure you have a label column in your df. If you don't and for internal purposes only OK to just use
-# non frontward facing label column (i.e. races not all spelled out).
-# Or you can create one with everything spelled out. 
-
-df<-df%>%
-  mutate(label=race)
-
 single_bar(df=df, 
-               indicator=indicator, 
-               title_text=title_text,
-               subtitle_text=subtitle_text,
-               caption_text=caption_text)
+           indicator=indicator, 
+           title_text=title_text
+               )
 
 # SINGLE BAR GRAPH W/ TOTAL LINE FUNCTION -------------------------------------
 
-df<-dbGetQuery(con, "SELECT * FROM analysis_graduation")
 
-title_text<-"High School Graduation Rates by Student Subgroup, <br>2023-24 School Year, Antelope Valley Union High School District"
-caption_text<-"Source: California Department of Education, Adjusted Cohort Graduation Rate and Outcome Data,
-2023-2024. Note: Rates are out of 100 students. AIAN stands for American Indian and Alaskan Native."
 
-single_bar_tot<-function(df, indicator, title_text, subtitle_text, caption_text){
+single_bar_tot<-function(df, indicator, title_text){
   
   # rename 'rate' column for function and arrange by rate descending
   df<-df%>%
@@ -262,6 +393,15 @@ single_bar_tot<-function(df, indicator, title_text, subtitle_text, caption_text)
  
  # set total value
  total_value<- subset(df, label=="Total")$rate
+ 
+ ## Set up subtitle text: This will be from the data dictionary
+ 
+ subtitle_text<-paste0(dict$indicator[dict$indicator_short==indicator])
+ 
+ # # set caption text to use values from the data dictionary
+ 
+ caption_text<-paste0("Source: Catalyst California calculations of ",dict$source[dict$indicator_short==indicator]," data, ", dict$year[dict$indicator_short==indicator],". ",dict$method_note[dict$indicator_short==indicator])
+ caption_text <- str_wrap(caption_text, width = 110)
  
  # Graph
  
@@ -337,21 +477,23 @@ single_bar_tot<-function(df, indicator, title_text, subtitle_text, caption_text)
 # EX) SINGLE BAR GRAPH W/ TOTAL LINE------------------------------------
 
 
-df<-dbGetQuery(con, "SELECT * FROM analysis_graduation")
+df<-dbGetQuery(con, "SELECT * FROM analysis_rent_burden")
 
-indicator<-"graduation"
+# check your table and do any prep necessary prior to visual
+
+df<-df%>%
+  rename(label=subgroup)%>%
+  race_recode()
+  
+
+indicator<-"Police Stops by Race"
 title_text<-"Findings based title"
-subtitle_text<-"High School Graduation Rates by Student Subgroup in Antelope Valley Union High School District"
-caption_text<-"Source: California Department of Education, Adjusted Cohort Graduation Rate and Outcome Data,
-2023-2024. Note: Rates are out of 100 students. AIAN stands for American Indian and Alaskan Native."
 
 # Apply function
 
 single_bar_tot(df=df, 
              indicator=indicator, 
-             title_text=title_text,
-           subtitle_text=subtitle_text,
-             caption_text=caption_text)
+             title_text=title_text)
 
 # Disconnect from postgres--------------------------
 dbDisconnect(con)
