27b20832aae6c19f82678e0c2e58ae41083b8fc7
jzhang514 <JZhang@catalystcalifornia.org>
2026-01-20 15:40:44 -0800

    committing progress on figuring out how to implement spanish translations to visuals

---
+++ Visuals/avuhsd_race_single_bar_sp.R
@@ -0,0 +1,73 @@
+# Create demographic chart for CTC report 
+# Author: CR
+
+
+# Set up workspace----------------------------------
+
+list.of.packages <- c("RPostgres", "tidyverse", "gt", "showtext", "scales", "forcats") 
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])] 
+
+if(length(new.packages)) install.packages(new.packages) 
+
+#### Loading Libraries ####
+library(RPostgres)
+library(tidyverse)
+library(gt)
+library(showtext)
+library(scales)
+library(forcats)
+
+#connect to postgres
+source("W:\\RDA Team\\R\\credentials_source.R")
+con_shared <- connect_to_db("rda_shared_data")
+con_ctc <- connect_to_db("cancel_the_contract")
+
+# connect to function script
+source("./Visuals/visual_fx.R")
+
+# AVUHSD DEMOGRAPHICS (RACE) BAR GRAPH ------------------------------------
+
+df<-dbGetQuery(con_ctc, "SELECT * FROM data.avuhsd_population_race")
+
+# first apply the race_sp function if you are visualizing something disaggregated by race
+# for it to work you need to rename your column that needs to be recoded to 'label'
+
+df<-df%>%
+  rename("label"="race")%>% # This is the column that needs to get renamed
+  race_sp() # apply race recoding
+
+
+Encoding(unique(df$label))
+
+
+df$label <- iconv(df$label, from = "latin1", to = "UTF-8")
+library(stringi)
+
+df$label <- stri_encode(df$label, from = "UTF-8", to = "latin1")
+
+df$label <- iconv(df$label, from = "latin1", to = "UTF-8")
+
+
+
+
+
+# NOTE: The indicator field needs to match the way it is in the data dictionary indicator_short column
+## i.e.) for suspensions by race, I need to set indicator== "Suspensions by race"
+
+indicator<-"Student Race"
+title_text<-"Los estudiantes latines, negros y blancos constituyen la mayoría de los estudiantes en el distrito escolar AVUHSD"
+subtitle_text<-"Población estudiantil de AVUHSD por raza"
+
+# Apply function
+
+single_bar_sp(df=df,
+           indicator=indicator,
+           title_text=title_text,
+           subtitle_text=subtitle_text
+)
+
+# Go check results in ./Visuals
+
+dbDisconnect(con_ctc)
+
